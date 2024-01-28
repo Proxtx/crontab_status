7a1d7840fd252baf01dcde93fa0f3032bce31996
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-01-28 23:26:11 +0100

    spawning and logging. Something is not working correctly.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,33 @@ use std::env;
 use std::io::Read;
 use std::process;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut args_it = env::args();
     args_it.next();
     let program = args_it.next().expect("Expected a program to be run");
 
-    let cmd = process::Command::new(program)
+    let mut cmd = process::Command::new(program)
         .args(args_it)
         .spawn()
         .expect("Unable to spawn child");
     //cmd.stdout.take().unwrap().read(buf);
+    if let Some(mut stdout) = cmd.stdout.take() {
+        tokio::spawn(async move {
+            loop {
+                let mut str = String::new();
+                stdout.read_to_string(&mut str).unwrap();
+                println!("{}", str);
+            }
+        });
+    }
+    if let Some(mut stderr) = cmd.stderr.take() {
+        tokio::spawn(async move {
+            loop {
+                let mut str = String::new();
+                stderr.read_to_string(&mut str).unwrap();
+                println!("{}", str);
+            }
+        });
+    }
 }
